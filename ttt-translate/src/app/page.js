e613e7c46f0aa2f0ed4d1262a7497312645d46d3
elliot <elliot.sedogbo@epitech.eu>
2024-02-27 10:50:07 +0100

    correction de fautes

--- ttt-translate/src/app/page.js
+++ ttt-translate/src/app/page.js
@@ -197,11 +197,11 @@ export default function Home() {
                 <li className="list-group-item"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                   viewBox="0 0 20 20">
                   <path fill="green" d="m14.83 4.89l1.34.94l-5.81 8.38H9.02L5.78 9.67l1.34-1.25l2.57 2.4z" />
-                </svg> Accurate and timely provision of subtitles htmlFor video content</li>
+                </svg> Accurate and timely provision of subtitles for video content</li>
                 <li className="list-group-item"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                   viewBox="0 0 20 20">
                   <path fill="green" d="m14.83 4.89l1.34.94l-5.81 8.38H9.02L5.78 9.67l1.34-1.25l2.57 2.4z" />
-                </svg> Enhancement of accessibility htmlFor diverse audiences</li>
+                </svg> Enhancement of accessibility for diverse audiences</li>
                 <li className="list-group-item"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                   viewBox="0 0 20 20">
                   <path fill="green" d="m14.83 4.89l1.34.94l-5.81 8.38H9.02L5.78 9.67l1.34-1.25l2.57 2.4z" />
