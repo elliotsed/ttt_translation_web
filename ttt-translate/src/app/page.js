7eac66325e092b6f8ecdb0c1f7c089a225aba4b6
elliot <elliot.sedogbo@epitech.eu>
2024-02-28 16:36:39 +0100

    modif+backup

--- ttt-translate/src/app/page.js
+++ ttt-translate/src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
             />
           </a>
 
-          <LanguageChanger />
+          <LanguageChanger className="d-none d-md-block" />
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
