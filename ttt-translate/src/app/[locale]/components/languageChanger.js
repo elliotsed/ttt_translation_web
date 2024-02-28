72926582ce3ae5c5072bb786263394db74fb3382
elliot <elliot.sedogbo@epitech.eu>
2024-02-28 16:43:58 +0100

    backup

--- ttt-translate/src/app/[locale]/components/languageChanger.js
+++ ttt-translate/src/app/[locale]/components/languageChanger.js
@@ -37,7 +37,7 @@ export default function LanguageChanger() {
     };
 
     return (
-        <select onChange={handleChange} value={currentLocale}>
+        <select class="form-select" aria-label="Default select example" onChange={handleChange} value={currentLocale}>
             <option value="en">English</option>
             <option value="fr">Français</option>
         </select>
