a1f7f4c914180302c763cbf6158210d4b82beabb
elliot <elliot.sedogbo@epitech.eu>
2024-02-29 17:12:25 +0100

    modif

--- ttt-translate/src/app/page.js
+++ ttt-translate/src/app/page.js
@@ -13,12 +13,14 @@ export default function Home() {
     message: ''
   });
 
+  const [submitted, setSubmitted] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (event) => {
+    setSubmitted(true);
     console.log(formData);
     event.preventDefault();
     try {
@@ -32,16 +34,19 @@ export default function Home() {
       const data = await response.json();
       if (response.ok) {
         alert(data.message);
-        setFormData({name:"", email:"", subject:"", message:""});
+        setFormData({ name: "", email: "", subject: "", message: "" });
+        setSubmitted(false);
       } else {
         alert(data.message);
-        setFormData({name:"", email:"", subject:"", message:""});
+        setFormData({ name: "", email: "", subject: "", message: "" });
+        setSubmitted(false);
 
       }
     } catch (error) {
       console.error('Error sending email:', error);
       alert('An error occurred while sending the email, please try again');
-      setFormData({name:"", email:"", subject:"", message:""});
+      setFormData({ name: "", email: "", subject: "", message: "" });
+      setSubmitted(false);
 
     }
   };
@@ -476,14 +481,14 @@ export default function Home() {
               <div className="row">
                 <div className="col-md-6">
                   <div className="form-outline mb-4">
-                    <input type="text" id="name" name="name" className="form-control" value={formData.name} onChange={handleChange} />
+                    <input type="text" id="name" name="name" className="form-control" value={formData.name} onChange={handleChange} required />
                     <label className="form-label" htmlFor="name">Your name</label>
                   </div>
                 </div>
 
                 <div className="col-md-6">
                   <div className="form-outline mb-4">
-                    <input type="email" id="email" name="email" className="form-control" value={formData.email} onChange={handleChange} />
+                    <input type="email" id="email" name="email" className="form-control" value={formData.email} onChange={handleChange} required />
                     <label className="form-label" htmlFor="email">Your email</label>
                   </div>
                 </div>
@@ -492,7 +497,7 @@ export default function Home() {
               <div className="row">
                 <div className="col-md-12">
                   <div className="form-outline mb-4">
-                    <input type="text" id="subject" name="subject" className="form-control" value={formData.subject} onChange={handleChange} />
+                    <input type="text" id="subject" name="subject" className="form-control" value={formData.subject} onChange={handleChange} required />
                     <label className="form-label" htmlFor="subject">Subject</label>
                   </div>
                 </div>
@@ -501,13 +506,14 @@ export default function Home() {
               <div className="row">
                 <div className="col-md-12">
                   <div className="form-outline mb-4">
-                    <textarea className="form-control" id="message" name="message" rows="4" value={formData.message} onChange={handleChange}></textarea>
+                    <textarea className="form-control" id="message" name="message" rows="4" value={formData.message} onChange={handleChange} required></textarea>
                     <label className="form-label" htmlFor="message">Your message</label>
                   </div>
                 </div>
               </div>
 
-              <button type="submit" className="btn text-white" style={{ backgroundColor: "#45526e" }}>Submit</button>
+              {!submitted && <button type="submit" className="btn text-white" style={{ backgroundColor: "#45526e" }}>Submit</button>}
+              {submitted && <p className='text-info'>Sending...</p>}
               <div className="status"></div>
             </form>
           </div>
