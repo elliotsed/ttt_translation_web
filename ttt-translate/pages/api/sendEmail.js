e9c290236d71aae03d4d98e95f33983406f84886
elliot <elliot.sedogbo@epitech.eu>
2024-02-29 17:33:35 +0100

    terminus

--- ttt-translate/pages/api/sendEmail.js
+++ ttt-translate/pages/api/sendEmail.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
             // Envoie d'un e-mail
             const info = await transporter.sendMail({
                 from: "Elliot SEDOGBO",
-                to: "sedotou98@gmail.com",
+                to: "tttmission@gmail.com",
                 subject: `New message from ${name}`,
                 html: `<h3>[SUBJECT]: ${subject}</h3><p>[SENDER]: ${name} - ${email}</p><p>${message}</p><p><strong>This message comes from ttt-lingua.vercel.app <br/> Contact directly the sender to answer.</strong></p>`,
             });
