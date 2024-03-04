309d929b0d4eeca0b839d1341a6997bbf7026820
elliot <elliot.sedogbo@epitech.eu>
2024-03-04 10:23:10 +0100

    backup

--- ttt-translate/src/app/page.js
+++ ttt-translate/src/app/page.js
@@ -466,8 +466,8 @@ export default function Home() {
                       d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054q.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992a4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054q.094-.558.31-.992q.217-.434.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992a4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z" />
                   </svg>
                 </div>
-                <p className="mb-0 mt-2 font-italic">
-                  &quot;The vision our team at TTT ENGLISH MISSION CENTER has so preciously over the years has been to maximize the speed and efficiency of all our services, customer experience, partnerships and responses to our clients while always topping quality. And it&apos;&apos;s so exciting to know that this vision will continue to drive us for the years to come&quot;
+                <p className="mb-0 mt-2 fst-italic">
+                  The vision our team at TTT ENGLISH MISSION CENTER has so preciously over the years has been to maximize the speed and efficiency of all our services, customer experience, partnerships and responses to our clients while always topping quality. And it&apos;s so exciting to know that this vision will continue to drive us for the years to come.
                 </p>
                 <footer className="blockquote-footer pt-4 mt-4 border-top">
                   Léon Bichat |
